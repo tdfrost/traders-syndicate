a639dc5fd42ed6ecd8a7373e0a214397bb45dcaf
Taylor <tdfrost004@gmail.com>
2024-05-02 15:48:46 -0500

    finish first pass on the Hero section for mobile and desktop

--- trade-syndicate/src/app/page.tsx
+++ trade-syndicate/src/app/page.tsx
@@ -5,24 +5,32 @@ const Home = () => {
   return (
     <div className="text-white">
       <main className="w-full my-12">
-        <div className="flex flex-col space-y-10">
-          <div className="w-full h-min space-y-4">
-            <h1 className="text-4xl  md:text-5xl font-semibold uppercase">
+        <div className="flex flex-col lg:flex-row space-y-10 md:items-center">
+          <div className="flex flex-col w-full h-min space-y-4">
+            <h1 className="text-4xl md:text-6xl font-semibold uppercase md:leading-[1.15]">
               Trade Together
               <br />
               Win Together
             </h1>
 
-            <p className="text-lg font-medium w-4/5">
+            <p className="text-lg font-base w-11/12 md:w-3/4 ">
               Learn to follow the rules that keep you in the Trade, and the art
               that helps you win.
             </p>
 
-            <p className="hidden md:block">
+            <p className="hidden md:block w-3/5">
               praesent tristique magna sit amet purus gravida quis blandit
               turpis cursus in hac habitasse platea dictumst quisque sagittis
               purus.
             </p>
+
+            <div className="hidden md:block py-2">
+              <PrimaryButton
+                buttonColor={ButtonType.primary}
+                text="Join Our Discord"
+                textCapitalized
+              />
+            </div>
           </div>
 
           <div className="flex items-center w-full">
@@ -35,11 +43,13 @@ const Home = () => {
             />
           </div>
 
-          <PrimaryButton
-            buttonColor={ButtonType.primary}
-            text="Join Our Discord"
-            textCapitalized
-          />
+          <div className="md:hidden">
+            <PrimaryButton
+              buttonColor={ButtonType.primary}
+              text="Join Our Discord"
+              textCapitalized
+            />
+          </div>
         </div>
 
         <div></div>
