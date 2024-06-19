fc56b36777f262d1fcaa21ac924f6736e4822579
Taylor <tdfrost004@gmail.com>
2024-06-19 00:32:25 -0500

    added asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const isProd = (process.env.NODE_ENV = "production")
 
 const nextConfig = {
+  assetPrefix: isprod ? "/traders-syndicate/" : "",
   basePath: isProd ? "/traders-syndicate" : "",
   output: "export",
   reactStrictMode: true,
