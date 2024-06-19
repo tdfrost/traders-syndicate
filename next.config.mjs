42d46cae4b73898a6c817a31f59cd565e78a7aec
Taylor <tdfrost004@gmail.com>
2024-06-18 23:47:44 -0500

    added github base path to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = (process.env.NODE_ENV = "production")
+
 const nextConfig = {
+  basePath: isProd ? "/traders-syndicate" : "",
   output: "export",
   reactStrictMode: true,
   images: {
